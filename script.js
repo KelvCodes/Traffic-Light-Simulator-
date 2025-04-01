ba075b0634d4ff0bd228274ba3ecd379fd4a73e9
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-04-01 15:59:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-
-
-// Function to provide vibration feedback (for mobile users)
-function vibrateFeedback() {
-    if (navigator.vibrate) {
-        navigator.vibrate([500, 200, 500]); // Vibrate pattern: 500ms, pause, 500ms
+  navigator.vibrate([500, 200, 500]); // Vibrate pattern: 500ms, pause, 500ms
     }
 }
 
